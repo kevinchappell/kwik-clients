b0ca78166cafe695684658fd2494d1af31f4c907
Kevin Chappell <kevin.b.chappell@gmail.com>
2015-01-19 09:25:12 -0800

    make at_a_glance static

--- inc/class.helpers.php
+++ inc/class.helpers.php
@@ -1,68 +1,72 @@
 <?php
 
-class K_CLIENTS_HELPERS extends KwikClients{
+class K_CLIENTS_HELPERS extends KwikClients
+{
 
-  function __construct() {
-      $this->name = "K_CLIENTS_HELPERS";
-   }
-
-  public function array_insert_at_position($array, $values, $pivot, $position = 'after'){
-
-    $offset = 0;
-    foreach($array as $key => $value){
-      ++$offset;
-      if ($key == $pivot){
-        break;
-      }
+    public function __construct()
+    {
+        $this->name = "K_CLIENTS_HELPERS";
     }
 
-    if ($position == 'before'){
-      --$offset;
+    public function array_insert_at_position($array, $values, $pivot, $position = 'after')
+    {
+
+        $offset = 0;
+        foreach ($array as $key => $value) {
+            ++$offset;
+            if ($key == $pivot) {
+                break;
+            }
+        }
+
+        if ($position == 'before') {
+            --$offset;
+        }
+
+        return array_slice($array, 0, $offset, true) + $values + array_slice($array, $offset, null, true);
     }
 
-    return array_slice($array, 0, $offset, TRUE) + $values + array_slice($array, $offset, NULL, TRUE);
-  }
-
-  // ADD NEW COLUMN
-  public function add_clients_columns($columns) {
-    $columns = self::array_insert_at_position($columns, array('featured_image' => __('Image')), 'cb');
-    return $columns;
-  }
-
-  public static function k_client_logo_text_filter( $translated_text, $untranslated_text, $domain ) {
-    global $post, $typenow, $current_screen;
-
-    if( is_admin() && 'clients' === $typenow )  {
-      switch( $untranslated_text ) {
-
-        case 'Insert into post':
-          $translated_text = __( 'Add to Client description','kwik' );
-        break;
-
-        case 'Set featured image':
-          $translated_text = __( 'Set Client logo','kwik' );
-        break;
-
-        case 'Set Featured Image':
-          $translated_text = __( 'Set Client Logo','kwik' );
-        break;
-
-        case 'Featured Image':
-          $translated_text = __( 'Client Logo','kwik' );
-        break;
-
-        case 'Enter title here':
-          $translated_text = __( 'Enter Client Name','kwik' );
-        break;
-       }
+    // ADD NEW COLUMN
+    public function add_clients_columns($columns)
+    {
+        $columns = self::array_insert_at_position($columns, array('featured_image' => __('Image')), 'cb');
+        return $columns;
     }
-    return $translated_text;
-  }
 
+    public static function k_client_logo_text_filter($translated_text, $untranslated_text, $domain)
+    {
+        global $post, $typenow, $current_screen;
 
-  public function clients_at_a_glance(){
-    Kwikutils::cpt_at_a_glance('clients');
-  }
+        if (is_admin() && 'clients' === $typenow) {
+            switch ($untranslated_text) {
 
+                case 'Insert into post':
+                    $translated_text = __('Add to Client description', 'kwik');
+                    break;
+
+                case 'Set featured image':
+                    $translated_text = __('Set Client logo', 'kwik');
+                    break;
+
+                case 'Set Featured Image':
+                    $translated_text = __('Set Client Logo', 'kwik');
+                    break;
+
+                case 'Featured Image':
+                    $translated_text = __('Client Logo', 'kwik');
+                    break;
+
+                case 'Enter title here':
+                    $translated_text = __('Enter Client Name', 'kwik');
+                    break;
+            }
+        }
+        return $translated_text;
+    }
+
+    public static function clients_at_a_glance()
+    {
+        Kwikutils::cpt_at_a_glance('clients');
+    }
 
 }
